d4fc23261b81e8a61d1fb79aab8546155e65518d
mobinnesari81 <mobinnesari81@gmail.com>
2021-11-07 11:17:27 +0330

    Fixing a problem

--- sorting_problem/quick_sort/quick_sort.py
+++ sorting_problem/quick_sort/quick_sort.py
@@ -27,8 +27,4 @@ def quick_sort(start, end, array):
 		quick_sort(start, p - 1, array)
 		quick_sort(p + 1, end, array)
 		
-array = [ 10, 7, 8, 9, 1, 5 ]
-quick_sort(0, len(array) - 1, array)
-
-print(f'Sorted array: {array}')
 	
