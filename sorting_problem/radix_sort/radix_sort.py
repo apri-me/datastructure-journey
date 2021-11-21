55a02ccabc1baf9855519217ce6a4438497fa21a
apri-me <alila2015b@gmail.com>
2021-11-21 12:30:54 +0330

    radix_sort added

---
+++ sorting_problem/radix_sort/radix_sort.py
@@ -0,0 +1,32 @@
+
+def find_digit(num, exp, b):
+    return (num // exp) % b
+
+
+def counting_sort(A, exp, k):
+    n = len(A)
+    answer = [None] * n
+    C = [0] * (k+1)
+    for i in range(n):
+        C[find_digit(A[i], exp, k+1)] += 1
+    for i in range(1, k + 1):
+        C[i] += C[i - 1]
+    for i in range(n-1, -1, -1):
+        ind = find_digit(A[i], exp, k+1)
+        answer[C[ind] - 1] = A[i]
+        C[ind] -= 1
+    for i in range(n):
+        A[i] = answer[i]
+
+
+def radix_sort(arr):
+    arr_max = max(arr)
+    exp = 1
+    while 0 < exp < arr_max:
+        counting_sort(arr, exp, 9)
+        exp *= 10
+
+
+# a = [121, 876, 321, 12, 565, 342, 171, 392, 717, 999]
+# radix_sort(a)
+# print(a)
