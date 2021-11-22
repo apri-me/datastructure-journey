22173dc0b2d0972fc6177cd241fffbbcdb150d3c
mobinnesari81 <mobinnesari81@gmail.com>
2021-11-22 10:02:14 +0330

    Rewrite heap sort file

--- sorting_problem/heap_sort/heap.py
+++ sorting_problem/heap_sort/heap.py
@@ -1,43 +1,20 @@
-# Implementation of heap functions
-
-import math
-
-
-def left(i):
-    return 2*i
-
-
-def right(i):
-    return i * 2 + 1
-
-
-def parent(i):
-    return i // 2
-
-
-def max_heapify(array, i):
+def heapify(arr, n, i):
     largest = i
-    l = left(i)
-    r = right(i)
-    if l <= len(array) - 1 and array[l] > array[i]:
+    l = 2 * i + 1
+    r = 2 * i + 2
+    if l < n and arr[largest] < arr[l]:
         largest = l
-    if r <= len(array) - 1 and array[r] > array[largest]:
+    if r < n and arr[largest] < arr[r]:
         largest = r
     if largest != i:
-        array[i], array[largest] = array[largest], array[i]
-        max_heapify(array, largest)
+        arr[i], arr[largest] = arr[largest], arr[i]
+        heapify(arr, n, largest)
 
 
-def build_max_heap(array):
-    for i in range(math.floor((len(array)-1)/2), 0, -1):
-        max_heapify(array, i)
-
-
-def heap_sort(array):
-    build_max_heap(array)
-    heap_size = len(array) - 1
-    for i in range(heap_size, 0, -1):
-        array[i], array[1] = array[1], array[i]
-        max_heapify(array, i)
-
-# Just consider index your array from 1 to n and using a list with n + 1 elements! First element is not useful at all.
+def heap_sort(arr):
+    n = len(arr)
+    for i in range(n//2 - 1, -1, -1):
+        heapify(arr, n, i)
+    for i in range(n-1, 0, -1):
+        arr[i], arr[0] = arr[0], arr[i]
+        heapify(arr, i, 0)
